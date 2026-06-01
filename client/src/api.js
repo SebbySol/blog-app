ba0412ec3fd286354ab9d58910574cbaeaf15a6e
sebastianangeles14 <sebastianangeles14@gmail.com>
2026-06-01 19:45:35 +0800

    change api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-app-xzt1.onrender.com", 
+  baseURL: "https://blog-app-xzt1.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
