c7e0e67b257f5dc06785f6b1c892e9c729ec3f87
sebastianangeles14 <sebastianangeles14@gmail.com>
2026-06-01 20:24:17 +0800

    fix mongodb

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -2,15 +2,15 @@
   <footer class="app-footer">
     <div class="container">
       <div class="row align-items-center py-4">
-        <!-- Left - Brand -->
+
         <div class="col-md-4">
           <h3 class="footer-brand">Daily<b>Write</b></h3>
         </div>
 
-        <!-- Right - Copyright -->
-        <div class="col-md-4 text-end">
+        <div class="col-md-8 text-end">
           <p class="mb-0 footer-copy">© 2026 DailyWrite. All rights reserved.</p>
         </div>
+        
       </div>
     </div>
   </footer>
