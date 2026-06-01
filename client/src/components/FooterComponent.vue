14949e5b50c6e051a6ec2dfbe95f2aa019117760
sebastianangeles14 <sebastianangeles14@gmail.com>
2026-06-01 20:19:59 +0800

    fix mongodb

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -7,13 +7,6 @@
           <h3 class="footer-brand">Daily<b>Write</b></h3>
         </div>
 
-        <!-- Center - Links -->
-        <div class="col-md-4 text-center">
-          <RouterLink to="/" class="footer-link me-3">Home</RouterLink>
-          <RouterLink to="/login" class="footer-link me-3">Login</RouterLink>
-          <RouterLink to="/register" class="footer-link">Register</RouterLink>
-        </div>
-
         <!-- Right - Copyright -->
         <div class="col-md-4 text-end">
           <p class="mb-0 footer-copy">© 2026 DailyWrite. All rights reserved.</p>
